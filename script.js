7a543a4ceacd48aca8110f9125a44d1a2e746677
Baqar Ali <sayedalintl@gmail.com>
2024-04-13 15:35:35 +0530

    update

--- script.js
+++ script.js
@@ -1,72 +1,48 @@
-const add_task_form = document.getElementById('add-task-form');
+const add_task_form = document.querySelector('button');
 const newtask = document.getElementById('new-task');
 const tasklist = document.getElementById('task-list');
 
-let json = JSON.parse(localStorage.getItem('todos'));
-// console.log(json);
+let todos =  JSON.parse(localStorage.getItem("todos")) || [];
+let str = "";
+let eleIndex;
 
-let index = 0;
-let jsonindex = null; // declared as index of json
-let str = ``;
-let findData;
-let edit = false;
-
-const showTodoitems = () => {
+function showTodos(){
     str = "";
-    json.forEach(ele => {
-        str += `<li onclick="ondelete(${ele.id})">${ele.text}</li> <i onclick="onEdit(${ele.id})" class="fa-solid fa-pen"></i>`
-    })
-    tasklist.innerHTML = str;
+    if(todos.length > 0){
+        todos.forEach(ele=>{
+            str += `<li style="list-style:none" onclick="ondelete('${ele.title}')">${ele.title}</li> <i onclick="onEdit('${ele.title}')" class="fa-solid fa-pen"></i>` 
+         });
+        tasklist.innerHTML = str;
+    }
 }
 
-if (json) {
-    showTodoitems()
-}
-else {
-    tasklist.innerHTML = "No items in Todos";
+if(todos){
+    showTodos()
 }
 
-document.querySelector('button').addEventListener('click', (e) => {
+add_task_form.addEventListener('click',(e)=>{
     e.preventDefault();
-    if (edit) {
-        let updateData = {...findData,text:newtask.value}
-
-        json.splice(jsonindex, 1, updateData);
-        localStorage.setItem('todos', JSON.stringify(json));
-        edit = false;
-        document.querySelector('button').innerHTML = "Add";
+    if(add_task_form.innerHTML === "Add"){
+        todos.push({title:newtask.value});
     }
-    else {
-        if (!json) {
-            json = [];
-            index = 0;
-            json.push({ id: index, text: newtask.value })
-            localStorage.setItem("todos", JSON.stringify(json))
-        }
-        else {
-            json = JSON.parse(localStorage.getItem('todos'));
-            index++;
-            json.push({ id: index, text: newtask.value })
-            localStorage.setItem("todos", JSON.stringify(json))
-        }
+    else{
+        todos.splice(eleIndex,1,{...todos[eleIndex],title:newtask.value});
+        add_task_form.innerHTML = "Add"
     }
-    showTodoitems();
+    localStorage.setItem("todos",JSON.stringify(todos));
+    showTodos()
     newtask.value = "";
 });
 
-
-function ondelete(i) {
-     jsonindex = json.findIndex(ele => ele.id == i)
-    json.splice(jsonindex, 1);
-    localStorage.setItem('todos', JSON.stringify(json));
-    showTodoitems();
+const ondelete = (title) =>{
+    eleIndex = todos.findIndex(ele=>ele.title === title);
+    todos.splice(eleIndex,1);
+    localStorage.setItem("todos",JSON.stringify(todos));
+    showTodos()
 }
 
-function onEdit(i) {
-     jsonindex = json.findIndex(ele => ele.id == i)
-    newtask.value = json[jsonindex].text;
-    edit = true
-    document.querySelector('button').innerHTML = "Edit";
-    findData = json.find(ele=>ele.text == newtask.value);
-};
-
+const onEdit = (title) =>{
+  eleIndex = todos.findIndex(ele=>ele.title === title);
+  newtask.value = todos[eleIndex].title;
+  add_task_form.innerHTML = "Edit"
+}
